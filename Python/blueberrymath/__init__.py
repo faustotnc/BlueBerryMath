92c4a958ddf013a583643ec1ed804d04caca24dd
roleyder <roleyder02@gmail.com>
2020-08-31 10:14:13 -0400

    + Added Sample Python Package | * Fix issue with typing modules | + Added New features to Python PyPi packages

--- Python/blueberrymath/__init__.py
+++ Python/blueberrymath/__init__.py
@@ -4,5 +4,5 @@ blueberrymath.
 Open Source Mathematical Package.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = 'Rogger Garcia | Fausto German'
--- Python/setup.py
+++ Python/setup.py
@@ -2,17 +2,18 @@ from setuptools import setup
 
 setup(
     name='blueberrymath',
-    version='0.1.0',
+    version='0.1.1',
     description='Open Source Mathematical Package',
     url='https://github.com/faustotnc/BlueBerryMath',
     author='Fausto German',
     author_email='faustotnc@gmail.com',
     license='MIT',
     packages=['blueberrymath'],
+    install_requires=['typing'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
